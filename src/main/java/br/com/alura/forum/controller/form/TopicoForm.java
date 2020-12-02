d8aeecd554105d2fc7eea2179dea747021ee46bf
Dimitri Gonçalves de Miranda <dgmiranda@stefanini.com>
2020-12-02 19:35:07 -0300

    Implementando handler de erro capiturados pelo bean validation usando o RestControllerAdvice

--- src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,7 +1,8 @@
 package br.com.alura.forum.controller.form;
 
 import javax.validation.constraints.NotBlank;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
@@ -9,10 +10,14 @@ import br.com.alura.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
-	@NotBlank
+	@NotBlank @NotNull @Size(min = 2, max = 500)
 	private String titulo;
+	@NotBlank @NotNull @Size(min = 2, max = 30)
 	private String mensagem;
+	@NotBlank @NotNull @Size(min = 2, max = 30)
 	private String nomeCurso;
+	
+	
 
 	public String getTitulo() {
 		return titulo;
