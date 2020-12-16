646a631639bb0856bba082a3caad66a46bd6ac31
Dimitri Miranda <dimitri.miranda@gmail.com>
2020-12-16 01:30:15 -0300

    implementando metodo de login usando o spring security

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,17 +8,23 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
 	
+	@Autowired
+	private AutenticacaoService  autenticacaoService;
+	
+	
 	//Configurações de autenticacao 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(auth);
+	 
+		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
+	
 	}
 	
 	//Configuracoes de Autorizacao
@@ -40,4 +47,13 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 		// TODO Auto-generated method stub
 		super.configure(web);
 	}
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.print(new BCryptPasswordEncoder().encode("123456"));
+		
+		
+	}
+	
 }
